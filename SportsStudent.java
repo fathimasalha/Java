7aa0f6ac87b484da851a42e243ae7e5366566b88
Fathima Salha NK <116252373+fathimasalha@users.noreply.github.com>
2023-04-18 14:45:17 +0530

    Add files via upload

---
+++ SportsStudent.java
@@ -0,0 +1,33 @@
+import java.util.*;
+interface  Student
+{
+int score=85;
+void displayScore();
+}
+interface Sports
+{
+int score=60;
+void displaySportsScore();
+}
+     
+class Result implements Student,Sports
+{
+public void displayScore()
+{
+System.out.println("Academic Score:"+Student.score);
+}
+public void displaySportsScore()
+{
+System.out.println("Sports Score:"+Sports.score);
+}
+}
+class SportsStudent
+{
+public static void main(String[]args)
+{
+Result r=new Result();
+r.displayScore();
+r.displaySportsScore();
+}
+}
+
