9c5849f760f0a211155909bb2f6a66312ecca050
Fathima Salha NK <116252373+fathimasalha@users.noreply.github.com>
2023-06-07 12:12:44 +0530

    Add files via upload

---
+++ 14-overloading/shapes/Shapes.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Shapes{
+int area(int l){
+return l*l;
+}
+int area(int l,int b){
+return l*b;
+}
+int area(int l,int b,int h){
+return((2*l*b)+(2*l*h)+(2*h*b));
+}
+public static void main(String[] args){	
+Shapes s=new Shapes();
+System.out.println("Area of square:"+s.area(6));
+System.out.println("Area of rectangle:"+s.area(6,3));
+System.out.println("Area of cuboid:"+s.area(1,3,5));
+}
+}
+
