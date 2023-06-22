8a53a6d4d9b19cafb6cb925a9547edce1d293c2a
Fathima Salha NK <116252373+fathimasalha@users.noreply.github.com>
2023-06-22 12:59:48 +0530

    Add files via upload

---
+++ 25-queue/PriorityQueues.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class PriorityQueues{
+public static void main(String args[]){
+PriorityQueue<String> queue=new PriorityQueue<String>();
+queue.add("a");
+queue.add("b");
+queue.add("c");
+queue.add("d");
+System.out.println("Queue Elements:");
+Iterator<String> itr=queue.iterator();
+while(itr.hasNext()){
+System.out.println(itr.next()+" ");
+}
+}
+}
+
+
