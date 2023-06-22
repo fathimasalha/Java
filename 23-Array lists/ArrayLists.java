5a7c078164f7f7ccac5d55fc2c72f8bb8d658431
Fathima Salha NK <116252373+fathimasalha@users.noreply.github.com>
2023-06-22 12:14:08 +0530

    Add files via upload

---
+++ 23-Array lists/ArrayLists.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class ArrayLists{
+public static void main(String[] args){
+ArrayList<String> al=new ArrayList<String>();
+System.out.println("Initial size of al:"+al.size());
+al.add("a");
+al.add("b");
+al.add("c");
+al.add("d");
+al.add("e");
+al.add("f");
+al.add(1,"a2");
+System.out.println("size of al after additions:"+al.size());
+System.out.println("contents of al:"+al);
+String str=al.get(2);
+al.set(2,str+"updated");
+System.out.println("contents of al after updation:"+al);
+al.remove("d");
+al.remove("2");
+System.out.println("size of al after deletion:"+al.size());
+System.out.println("contents of al:"+al);
+}
+}
+
+
