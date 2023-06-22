66ccacc4db24f9a3a473536f675773a3d26e610a
Fathima Salha NK <116252373+fathimasalha@users.noreply.github.com>
2023-06-22 13:00:03 +0530

    Add files via upload

---
+++ 26-LinkedHashSets/LinkedHashSets.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class LinkedHashSets{
+public static void main(String args[]){
+LinkedHashSet<String> set=new LinkedHashSet<String>();
+set.add("a");
+set.add("b");
+set.add("c");
+set.add("d");
+Iterator<String> itr=set.iterator();
+System.out.println("The Elements in the set:");
+while(itr.hasNext()){
+System.out.println(itr.next()+" ");
+}
+}
+}
+
